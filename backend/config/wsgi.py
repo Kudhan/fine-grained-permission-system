534e8887c707400768f08484cabd8fa7dd6ba95e
Kudhan <kudhanshaik03@gmail.com>
2026-02-21 16:17:13 +0530

    fixed the import changes

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
-from django.config.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
